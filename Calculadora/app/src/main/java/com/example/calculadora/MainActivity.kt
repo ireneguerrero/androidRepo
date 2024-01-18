67db82ad6ea25931574780926eed6e0a186834d8
ireneguerrero <irene2g222@gmail.com>
2024-01-18 18:39:41 +0100

    Calculadora2.0LaVenganza

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.calculadora
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun performOperation(view: View) {
+        val operador1 = findViewById<EditText>(R.id.editTextOperand1).text.toString()
+        val operador2 = findViewById<EditText>(R.id.editTextOperand2).text.toString()
+
+        if (operador1.isNotEmpty() && operador2.isNotEmpty()) {
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("Operador 1", operador1.toDouble())
+            intent.putExtra("Operador 2", operador2.toDouble())
+
+            when (view.id) {
+                R.id.btnSuma -> intent.putExtra("Operacion", "+")
+                R.id.btnResta -> intent.putExtra("Operacion", "-")
+                R.id.btnMultiplicar -> intent.putExtra("Operacion", "*")
+                R.id.btnDividir -> intent.putExtra("Operacion", "/")
+            }
+
+            startActivity(intent)
+        }
     }
 }
--- Calculadora/app/src/main/java/com/example/calculadora/ResultActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/ResultActivity.kt
@@ -2,10 +2,28 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val operador1 = intent.getDoubleExtra("Operador 1", 0.0)
+        val operador2 = intent.getDoubleExtra("Operador 2", 0.0)
+        val operacion = intent.getStringExtra("Operacion")
+
+        val result = performOperation(operador1, operador2, operacion)
+        findViewById<TextView>(R.id.textViewResult).text = "Resultado: $result"
+    }
+
+    private fun performOperation(operador1: Double, operador2: Double, operacion: String?): Double {
+        return when (operacion) {
+            "+" -> operador1 + operador2
+            "-" -> operador1 - operador2
+            "*" -> operador1 * operador2
+            "/" -> if (operador2 != 0.0) operador1 / operador2 else Double.NaN
+            else -> 0.0
+        }
     }
 }
