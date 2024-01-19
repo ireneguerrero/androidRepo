0cb48462de4725444e24f000e59ce702d54c529c
ireneguerrero <irene2g222@gmail.com>
2024-01-19 20:49:41 +0100

    Muchas cosas no puedo más

---
+++ Apuntes2Trimestre/app/src/main/java/com/example/apuntes2trimestre/apuntes2/LineaHorizontalYVerticalConBotones.kt
@@ -0,0 +1,12 @@
+package com.example.apuntes2trimestre.apuntes2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.apuntes2trimestre.R
+
+class LineaHorizontalYVerticalConBotones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linea_horizontal_yvertical_con_botones)
+    }
+}
